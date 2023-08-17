515048c535cc63416c6eb051a45a60ba7b0e9ee7
Duarte M <64815777+ozzypt@users.noreply.github.com>
2023-08-17 20:18:05 +0100

    first commit

---
+++ src/pages/Favorites.js
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
+function FavoritesPage() {
+    const favoriteCtx = useContext(FavoritesContext)
+
+    let content
+
+    if(favoriteCtx.totalFavorites === 0){
+        content = <p>You have no Favorites yet!</p>
+    } else {
+        content = <MeetupList meetups={favoriteCtx.favorites}/>
+    }
+
+
+    return (
+        <section>
+            <h1>My Favorites</h1>
+            {content}
+
+        </section>
+    )
+}
+
+export default FavoritesPage;
